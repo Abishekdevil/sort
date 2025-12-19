b63cb2395bfa4fc0d9a4128c33c071269a2d9935
Abishekdevil <abishekcholan07@gmail.com>
2025-12-19 17:29:54 +0530

    bubble sort question solved

---
+++ bubble.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = []
+
+
+for i in range(n):
+    arr.append(int(input()))
+
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print("Sorted array:")
+print(arr)
